7d140c8e8ebc27827c158ec8e20dd6dcceaf202d
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2026-02-04 16:12:35 +0100

    Some fixes

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,7 @@
+export FLYCTL_INSTALL="/Users/mossa/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export ANTHROPIC_AUTH_TOKEN=ollama
+export ANTHROPIC_BASE_URL=http://localhost:11434
+
 docker compose up --build
